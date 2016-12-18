91db0a0ce449ed90671b78c3f6f73b1870d3ed0f
nickyhaflinger <noah.healy@gmail.com>
2016-12-18 15:32:40 -0500

    Add files via upload

--- core.clj
+++ core.clj
@@ -282,6 +282,8 @@
 (defn slice [aa bb]
   (if (map? aa) (map #(get aa %) bb) (map #(nth aa %) bb)))
 
+(defn sum-num [aa] (reduce + 0 (filter number? aa)))
+
 (defn fill-in [aa position goal top bottom]
   (let
       [r-c-d [#{0 1 2 3} #{4 5 6 7} #{8 9 10 11} #{12 13 14 15} #{0 4 8 12} #{1 5 9 13} #{2 6 10 14} #{3 7 11 15} #{0 5 10 15} #{3 6 9 12}]
